372bcc80ec8180dc3cdf4d5d8c8524703e86a309
Kamesas <semakaleksandr2014@gmail.com>
2022-12-10 11:30:07 +0200

    Init and add tailwind

---
+++ app/routes/index.tsx
@@ -0,0 +1,21 @@
+export default function Index() {
+  return (
+    <div>
+      <h1 className="text-3xl uppercase font-bold text-center pt-1 bg-slate-300">
+        Hello world!
+      </h1>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+            className="hover:text-slate-600"
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
