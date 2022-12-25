b9ec9bfeedd0f3a43167866dd7105408a861bd2c
Kamesas <semakaleksandr2014@gmail.com>
2022-12-25 11:10:35 +0200

    add Exercises

--- app/routes/workout/index.tsx
+++ app/routes/workout/index.tsx
@@ -1,8 +1,15 @@
-import type { LoaderFunction } from "@remix-run/node";
+import type { ActionFunction, LoaderFunction } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import {
+  Form,
+  useActionData,
+  useLoaderData,
+  useSubmit,
+} from "@remix-run/react";
+import { useState } from "react";
 import { requireUserId } from "~/utils/auth.server";
-import { getExercisesById } from "~/utils/training.server";
+import { createExercise, getExercisesById } from "~/utils/training.server";
 
 export const loader: LoaderFunction = async ({ request }) => {
   const userId = await requireUserId(request);
@@ -10,8 +17,60 @@ export const loader: LoaderFunction = async ({ request }) => {
   return json({ exercise, userId });
 };
 
+export const action: ActionFunction = async ({ request }) => {
+  const userId = await requireUserId(request);
+
+  const form = await request.formData();
+  const message = form.get("message");
+
+  if (typeof message !== "string") {
+    return json({ error: `Invalid Form Data` }, { status: 400 });
+  }
+
+  if (!message.length) {
+    return json({ error: `Please provide a message.` }, { status: 400 });
+  }
+
+  await createExercise({ name: "push ups", userId, value: message });
+
+  return redirect("/workout");
+};
+
 export default function Training() {
+  const actionData = useActionData();
   const { exercise, userId } = useLoaderData();
-  console.log("user", exercise, userId);
-  return <div className="Training">Training</div>;
+  const [formError] = useState(actionData?.error || "");
+  const [formData, setFormData] = useState({
+    message: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    field: string
+  ) => {
+    setFormData((data) => ({ ...data, [field]: e.target.value }));
+  };
+
+  console.log("user", exercise, userId, actionData);
+  return (
+    <div className="Training">
+      <h1>Training</h1>
+
+      <Form method="post">
+        <textarea
+          name="message"
+          className="w-full rounded-xl h-40 p-4"
+          value={formData.message}
+          onChange={(e) => handleChange(e, "message")}
+          placeholder={`Set value`}
+        />
+        <button
+          type="submit"
+          className="rounded-xl bg-yellow-300 font-semibold text-blue-600 w-80 h-12 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
+        >
+          Send
+        </button>
+      </Form>
+    </div>
+  );
 }
--- app/utils/training.server.ts
+++ app/utils/training.server.ts
@@ -1,30 +1,36 @@
 import { prisma } from "./prisma.server";
 
 export const getExercisesById = async (userId: string) => {
-  return await prisma.user.findUnique({
+  return await prisma.exercises.findMany({
     where: {
-      id: userId,
+      userId,
+      name: {
+        equals: "push ups",
+      },
+      createdAt: {
+        gte: new Date("2022-12-25"),
+        lt: new Date("2022-12-26"),
+      },
     },
-    select: { exercises: true },
   });
 };
 
-export const createKudo = async (
-  message: string,
-  userId: string,
-  recipientId: string
-) => {
-  await prisma.kudo.create({
+export const createExercise = async ({
+  name,
+  userId,
+  value,
+}: {
+  name: string;
+  userId: string;
+  value: string;
+}) => {
+  await prisma.exercises.create({
     data: {
-      message,
-      author: {
+      name,
+      value,
+      user: {
         connect: {
           id: userId,
-        },
-      },
-      recipient: {
-        connect: {
-          id: recipientId,
         },
       },
     },
